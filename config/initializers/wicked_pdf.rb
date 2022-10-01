437c692815650cc3ef2b4e808ff3fe13f612f64d
jonnathanbruno100 <jonnathanbruno@gmail.com>
2022-10-01 11:54:12 -0300

    Add index application

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,8 @@
 WickedPdf.config ||= {}
 WickedPdf.config.merge!({
+  formats: [:html]
 }) 
 
-#WickedPdf.config = {
-#  :exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
-#}
+WickedPdf.config = {
+  :exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+}
