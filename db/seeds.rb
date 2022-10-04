46d5893fbc73c19c0f8b4d136fd691a859fdb861
jonnathanbruno100 <jonnathanbruno@gmail.com>
2022-10-04 13:48:09 -0300

    Ajustes em Listar Palestra

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,28 @@
 p "iniciando...."
 
-    Parish.create!(
-        name: "Paróquia de Sant'Ana e São Joaquim"
+    Circle.create!(
+        color: "Vermelho"
     )
 
-    Parish.create!(
-        name: "Paróquia de Santo Afonso"
+    Circle.create!(
+        color: "Verde"
     )
 
-    Parish.create!(
-        name: "Paróquia de Nísia"
+    Circle.create!(
+        color: "Azul"
     )
 
-60.times do
-    Follower.create!(
-        name: "Seguidor #{rand(10.000)}",
-        email: "seguidor@gmail.com",
-        birth: rand(365).days.ago,
-        address: "endereço de teste, nº 20",
-        type_date: "Seguidor",
-        parish_id: 1,
-        contact: "(84) 99182-8410"
+    Circle.create!(
+        color: "Amrelo"
     )
-end
 
-60.times do
-    Participant.create!(
-        name: "Seguimista #{rand(10.000)}",
-        email: "seguimista@gmail.com",
-        birth: rand(365).days.ago,
-        address: "endereço de teste, nº 20",
-        type_date: "Seguimista",
-        parish_id: 1,
-        contact: "(84) 99182-8410"
+    Circle.create!(
+        color: "Rosa"
     )
-end
 
-
-16.times do
-    Speaker.create!(
-        name: "Palestrante #{rand(10.000)}",
-        email: "palestrante@gmail.com",
-        birth: rand(365).days.ago,
-        address: "endereço de teste, nº 20",
-        type_date: "Palestrante",
-        parish_id: 1,
-        contact: "(84) 99182-8410"
+    Circle.create!(
+        color: "Laranja"
     )
-end
+
 
 p "terminou!"
