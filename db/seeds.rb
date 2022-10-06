6453c94607b36f031c3c819fae349a126a8d64f8
jonnathanbruno100 <jonnathanbruno@gmail.com>
2022-10-06 16:01:35 -0300

    Atualização da SEED para alimentar o BD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
 p "iniciando...."
 
+    Parish.create!(
+        name: "Paróquia de Sant'Ana e São Joaquim"
+    )
+
+    Parish.create!(
+        name: "Paróquia de Santo Afonso"
+    )
+
+    Parish.create!(
+        name: "Paróquia de Nísia"
+    )
+
     Circle.create!(
         color: "Vermelho"
     )
@@ -13,7 +25,7 @@ p "iniciando...."
     )
 
     Circle.create!(
-        color: "Amrelo"
+        color: "Amarelo"
     )
 
     Circle.create!(
@@ -23,6 +35,45 @@ p "iniciando...."
     Circle.create!(
         color: "Laranja"
     )
+
+    60.times do
+        Follower.create!(
+            name: "Seguidor Fulano de Tal #{rand(10.000)}",
+            email: "seguidor@gmail.com",
+            birth: rand(365).days.ago,
+            address: "endereço de teste, nº 20",
+            type_date: "Seguidor",
+            parish_id: 1,
+            contact: "(84) 99182-8410"
+        )
+    end
+
+    60.times do
+        Participant.create!(
+            name: "Seguimista Fulano de Tal #{rand(10.000)}",
+            email: "seguimista@gmail.com",
+            birth: rand(365).days.ago,
+            address: "endereço de teste, nº 20",
+            surname: "Seguimista",
+            type_date: "Seguimista",
+            parish_id: 1,
+            contact: "(84) 99182-8410"
+        )
+    end
+
+    16.times do
+        Speaker.create!(
+            name: "Palestrante Fulano de Tal #{rand(10.000)}",
+            email: "palestrante@gmail.com",
+            birth: rand(365).days.ago,
+            address: "endereço de teste, nº 20",
+            type_date: "Palestrante",
+            parish_id: 1,
+            contact: "(84) 99182-8410"
+        )
+    end
+
+
 
 
 p "terminou!"
