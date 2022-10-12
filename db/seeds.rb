2efef96f2b8a7c274e36f64971519e6f99eb27ad
jonnathanbruno100 <jonnathanbruno@gmail.com>
2022-10-12 07:52:14 -0300

    Atualização Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,49 +36,5 @@ p "iniciando...."
         color: "Laranja"
     )
 
-    60.times do
-        Follower.create!(
-            name: "Seguidor Fulano de Tal #{rand(10.000)}",
-            email: "seguidor@gmail.com",
-            birth: rand(365).days.ago,
-            address: "endereço de teste, nº 20",
-            type_date: "Seguidor",
-            parish_id: 1,
-            contact: "(84) 99182-8410"
-        )
-    end
-
-    60.times do
-        Participant.create!(
-            name: "Seguimista Fulano de Tal #{rand(10.000)}",
-            email: "seguimista@gmail.com",
-            birth: rand(365).days.ago,
-            address: "endereço de teste, nº 20",
-            surname: "Seguimista",
-            type_date: "Seguimista",
-            parish_id: 1,
-            contact: "(84) 99182-8410"
-        )
-    end
-
-    16.times do
-        Speaker.create!(
-            name: "Palestrante Fulano de Tal #{rand(10.000)}",
-            email: "palestrante@gmail.com",
-            birth: rand(365).days.ago,
-            address: "endereço de teste, nº 20",
-            type_date: "Palestrante",
-            parish_id: 1,
-            contact: "(84) 99182-8410"
-        )
-    end
-
-    10.times do
-        Lecture.create!(
-            name: "Nome de Palastra para Teste #{rand(10.000)}",
-            date_lecture: rand(365).days.ago,
-            time_lecture: rand(365).days.ago
-        )
-    end
 
 p "terminou!"
