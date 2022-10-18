7de315ada67fd443565627fa168bf89f88293032
jonnathanbruno100 <jonnathanbruno@gmail.com>
2022-10-18 09:15:48 -0300

    Quadrante 2019 digitalizado

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,11 +1,20 @@
 class DocumentsController < ApplicationController
-    def download_pdf
+    def download_pdf_2015
         send_file(
           "#{Rails.root}/public/quadrante2015.pdf",
           filename: "QUADRANTE_SEGUE_ME_2015.pdf",
           type: "application/pdf"
         )
     end
+
+    def download_pdf_2019
+      send_file(
+        "#{Rails.root}/public/quadrante2019.pdf",
+        filename: "QUADRANTE_SEGUE_ME_2019.pdf",
+        type: "application/pdf"
+      )
+  end
+
     def index
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get "/mountings/followers", to: "mountings#followers"
   get "/mountings/speakers", to: "mountings#speakers"
   get "/mountings/quadrante", to: "mountings#quadrante"
-  get "documents/download_pdf"
+  get "documents/download_pdf_2015"
+  get "documents/download_pdf_2019"
   get "documents", to: "documents#index"
 
   resources :mountings, only: [:index, :followers, :speakers, :quadrante]
