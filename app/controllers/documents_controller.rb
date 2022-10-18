63677457c527cec5c8d62dfc0e7c93631018048e
jonnathanbruno100 <jonnathanbruno@gmail.com>
2022-10-18 14:37:39 -0300

    quadrante2018

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -7,6 +7,14 @@ class DocumentsController < ApplicationController
         )
     end
 
+    def download_pdf_2018
+      send_file(
+        "#{Rails.root}/public/quadrante2018.pdf",
+        filename: "QUADRANTE_SEGUE_ME_2018.pdf",
+        type: "application/pdf"
+      )
+  end
+
     def download_pdf_2019
       send_file(
         "#{Rails.root}/public/quadrante2019.pdf",
