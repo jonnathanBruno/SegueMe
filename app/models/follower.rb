90985e119534a1340b41564c2a9151989174525f
jonnathanbruno100 <jonnathanbruno@gmail.com>
2022-10-19 10:20:20 -0300

    ajustes na quebra do quadrante

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -2,7 +2,10 @@ class Follower < ApplicationRecord
     has_one :mounting, dependent: :destroy
     belongs_to :parish, optional: true
   
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true, 
+              :length =>{ :maximum => 45,
+                          :too_long => ": %{count} caracteres é o máximo permitido" }
+
     validates :address, presence: true
     validates :birth, presence: true
 
